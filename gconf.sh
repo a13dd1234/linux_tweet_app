d96042a8b05f4e033d97a4315447e0fd49bb13b1
a13dd1234 <53269307+a13dd1234@users.noreply.github.com>
2020-05-24 18:42:59 +0000

    gconf.sh - add Docker id

--- gconf.sh
+++ gconf.sh
@@ -1,5 +1,19 @@
+
+# Git Config:
+
+echo "*** Git Config: ***"
+echo ""
 git config --global user.name "a13dd1234"
 git config --global user.email "53269307+a13dd1234@users.noreply.github.com"
 git config --global user.name
 git config --global user.email
+echo ""
+
+echo "*** Docker Config: ***"
+echo ""
+export DOCKERID="a13dd1234"
+echo "DOCKERID:"
+echo $DOCKERID
+echo ""
+echo ""
 
