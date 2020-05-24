30f3a1ca4d386eb55ecf5a567f465c6b3b973ef0
a13dd1234 <53269307+a13dd1234@users.noreply.github.com>
2020-05-24 18:13:05 +0000

    add gconf.sh  - config git name / email

---
+++ gconf.sh
@@ -0,0 +1,5 @@
+git config --global user.name "a13dd1234"
+git config --global user.email "53269307+a13dd1234@users.noreply.github.com"
+git config --global user.name
+git config --global user.email
+
